5093cd32ac4ab4b07941233b7c227054a9d82a95
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-02-14 22:25:47 +0000

    fixing score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,19 +7,19 @@ class GamesController < ApplicationController
     10.times do
       @letters << ('A'..'Z').to_a.sample
     end
-    return @letters
   end
 
   def score
+    from_letters = params[:from_letters].chars
     attempt = params[:word]
     filepath = "https://wagon-dictionary.herokuapp.com/#{attempt}"
     serialized_dictionary = URI.open(filepath).read
     result = JSON.parse(serialized_dictionary)
     if result["found"] == true
-      if attempt.chars.all? { |letter| attempt.count(letter) <= new.count(letter)}
+      if attempt.chars.all? { |letter| attempt.count(letter) <= from_letters.count(letter) }
         @score = "Congratulations! #{attempt.upcase} is a valid English word!"
       else
-        @score = "Sorry but #{attempt.upcase} can't be built out of #{new.join(", ")}"
+        @score = "Sorry but #{attempt.upcase} can't be built out of #{from_letters.join(", ")}"
       end
     else
       @score = "Sorry but #{attempt.upcase} does not seem to be a valid English word..."
